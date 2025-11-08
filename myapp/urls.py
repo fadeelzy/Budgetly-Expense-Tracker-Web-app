00eda860d59fb4067e9a47d0444307949fb081c2
Fadilah Abdulkadir <fadeelzy@gmail.com>
2025-11-08 10:28:02 +0100

    modify urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
-    path('add-expense', views.add_expense, name="add-expense"),
-    path('summary', views.summary, name="summary")
+    path('add-expense/', views.add_expense, name="add-expense"),
+    path('summary/', views.summary, name="summary")
 
 ]
